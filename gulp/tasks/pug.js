db222c12f237c499ea742bbcc3893cf273bc0367
Serj.Korneev <korneev.s@merlion.ru>
2018-04-18 18:33:10 +0300

    add base style

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -3,16 +3,7 @@
 module.exports = function() {
   $.gulp.task('pug', function() {
     return $.gulp.src('./source/template/pages/*.pug')
-      .pipe($.gp.pug({ 
-        pretty: true,
-        options: {
-          filters: {
-            declination: function (n) {
-              return ['мышь', 'мыши', 'мышей'][n % 10 === 1 && n % 100 !== 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
-            }
-          }
-        }
-      }))
+      .pipe($.gp.pug({ pretty: true }))
       .on('error', $.gp.notify.onError(function(error) {
         return {
           title: 'Pug',
